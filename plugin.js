7d2159ea49255d30543203dd15c7049937667d42
Roman Ryzhikov <general@ya.ru>
2021-11-06 02:03:11 +0200

    webp to avif

--- plugin.js
+++ plugin.js
@@ -1,77 +1,50 @@
+let postcss = require("postcss");
+
 const DEFAULT_OPTIONS = {
   modules: false,
-  noAvifClass: 'no-avif',
-  avifClass: 'avif',
-  addNoJs: true,
-  noJsClass: 'no-js',
-  rename: oldName => {
-    return oldName.replace(/\.(jpe?g|png)/gi, '.avif')
-  }
-}
+  noAvifClass: "no-avif",
+  avifClass: "avif",
+};
 
-module.exports = (opts = {}) => {
-  let { modules, noAvifClass, avifClass, addNoJs, noJsClass, rename } = {
-    ...DEFAULT_OPTIONS,
-    ...opts
-  }
-
-  function removeHtmlPrefix(className) {
-    return className.replace(/html ?\./, '')
-  }
+module.exports = postcss.plugin("avif-in-css/plugin", (opts) => {
+  let { modules, noAvifClass, avifClass } = { ...DEFAULT_OPTIONS, ...opts };
 
   function addClass(selector, className) {
-    let generatedNoJsClass
-    let initialClassName = className
-    if (className.includes('html')) {
-      className = removeHtmlPrefix(className)
-    }
     if (modules) {
-      className = `:global(.${className})`
-      generatedNoJsClass = `:global(.${noJsClass})`
+      className = `:global(.${className})`;
     } else {
-      className = `.${className}`
-      generatedNoJsClass = `.${noJsClass}`
+      className = `.${className}`;
     }
-    if (selector.includes('html')) {
-      selector = selector.replace(/html[^ ]*/, `$& body${className}`)
+    if (selector.includes("html")) {
+      return selector.replace(/html[^ ]*/, `$& body${className}`);
     } else {
-      selector = `body${className} ` + selector
+      return `body${className} ` + selector;
     }
-    if (addNoJs && initialClassName === noAvifClass) {
-      selector +=
-        ', ' +
-        selector.split(`body${className}`).join(`body${generatedNoJsClass}`)
-    }
-    return selector
   }
-  return {
-    postcssPlugin: 'avif-in-css/plugin',
-    Declaration(decl) {
-      if (/\.(jpe?g|png)(?!(\.avif|.*[&?]format=avif))/i.test(decl.value)) {
-        let rule = decl.parent
-        if (rule.selector.includes(`.${removeHtmlPrefix(noAvifClass)}`)) return
-        let avif = rule.cloneAfter()
-        avif.each(i => {
-          if (i.prop !== decl.prop && i.value !== decl.value) i.remove()
-        })
-        avif.selectors = avif.selectors.map(i => addClass(i, avifClass))
-        avif.each(i => {
-          if (
-            rename &&
-            Object.prototype.toString.call(rename) === '[object Function]'
-          ) {
-            i.value = rename(i.value)
-          }
-        })
-        let noAvif = rule.cloneAfter()
-        noAvif.each(i => {
-          if (i.prop !== decl.prop && i.value !== decl.value) i.remove()
-        })
-        noAvif.selectors = noAvif.selectors.map(i => addClass(i, noAvifClass))
-        decl.remove()
-        if (rule.nodes.length === 0) rule.remove()
+
+  return (root) => {
+    root.walkDecls((decl) => {
+      if (/\.(jpg|png)/i.test(decl.value)) {
+        let rule = decl.parent;
+        if (rule.selector.indexOf(`.${noAvifClass}`) !== -1) return;
+        let avif = rule.cloneAfter();
+        avif.each((i) => {
+          if (i.prop !== decl.prop && i.value !== decl.value) i.remove();
+        });
+        avif.selectors = avif.selectors.map((i) => addClass(i, avifClass));
+        avif.each((i) => {
+          i.value = i.value.replace(/\.(jpg|png)/gi, ".avif");
+        });
+        let noAvif = rule.cloneAfter();
+        noAvif.each((i) => {
+          if (i.prop !== decl.prop && i.value !== decl.value) i.remove();
+        });
+        noAvif.selectors = noAvif.selectors.map((i) =>
+          addClass(i, noAvifClass)
+        );
+        decl.remove();
+        if (rule.nodes.length === 0) rule.remove();
       }
-    }
-  }
-}
-module.exports.postcss = true
+    });
+  };
+});
